26871eed24fcddf89cbdb8e262daf1f1e4ca2e9f
vitalyvitmens <otm_@tut.by>
2023-10-02 17:17:43 +0300

    add data in project

--- public/app.js
+++ public/app.js
@@ -5,24 +5,63 @@ document.addEventListener('click', (event) => {
     remove(id).then(() => {
       event.target.closest('li').remove()
     })
-  } else if (event.target.dataset.type === 'edit') {
+  }
+
+  if (event.target.dataset.type === 'edit') {
     const newTitle = prompt('Введите новое название')
 
     if (newTitle) {
-      edit(id, newTitle).then(() => ({
-        id: id,
-        title: newTitle,
-      }))
+      edit({ title, id }).then(() => {
+        event.target.dataset.title = newTitle
+      })
     }
   }
 })
+
+// if (event.target.dataset.type === 'edit') {
+//   const $task = event.target.closest('li')
+//   const title = event.target.dataset.title
+//   const initialHtml = $task.innerHTML
+
+//   $task.innerHTML = `
+//     <input type="text" value="${title}">
+//     <div>
+//       <button class="btn btn-success" data-type="save">Сохранить</button>
+//       <button class="btn btn-danger" data-type="cancel">Отменить</button>
+//     </div>
+//   `
+
+//   const taskListener = ({ target }) => {
+//     if (target.dataset.type === 'cancel') {
+//       $task.innerHTML = initialHtml
+//       $task.removeEventListener('click', taskListener)
+//     }
+//     if (target.dataset.type === 'save') {
+//       const title = $task.querySelector('input').value
+//       edit({ title, id }).then(() => {
+//         $task.innerHTML = initialHtml
+//         $task.querySelector('span').innerText = title
+//         $task.querySelector('[data-type=edit]').dataset.title = title
+//         $task.removeEventListener('click', taskListener)
+//       })
+//     }
+//   }
+
+//   $task.addEventListener('click', taskListener)
+// }
+// })
 
 async function remove(id) {
   await fetch(`/${id}`, { method: 'DELETE' })
 }
 
-async function edit(id, title) {
-  console.log('From app.js function edit(id):', id)
-  console.log('From app.js function edit(title):', title)
-  await fetch(`/${id}`, { method: 'PUT' })
+async function edit(newNote) {
+  await fetch(`/${newNote.id}`, {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newNote),
+  })
 }
