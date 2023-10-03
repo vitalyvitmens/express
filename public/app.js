5bece7a2358f00471d891a68ab24eef9300914b7
vitalyvitmens <otm_@tut.by>
2023-10-03 16:37:52 +0300

    add data in project

--- public/app.js
+++ public/app.js
@@ -1,47 +1,28 @@
 document.addEventListener('click', (event) => {
-  if (event.target.dataset.type === 'remove') {
-    const id = event.target.dataset.id
+  const id = event.target.dataset.id
 
+  if (event.target.dataset.type === 'remove') {
     remove(id).then(() => {
       event.target.closest('li').remove()
     })
   }
 
   if (event.target.dataset.type === 'edit') {
-    const $task = event.target.closest('li')
-    const id = event.target.dataset.id
-    const title = event.target.dataset.title
-    const initialHtml = $task.innerHTML
+    const title = prompt('Введите новое название')
 
-    $task.innerHTML = `
-      <input type="text" value="${title}">
-      <div>
-        <button class="btn btn-success" data-type="save">Сохранить</button>
-        <button class="btn btn-danger" data-type="cancel">Отменить</button>
-      </div>
-    `
-
-    const taskListener = ({ target }) => {
-      if (target.dataset.type === 'cancel') {
-        $task.innerHTML = initialHtml
-        $task.removeEventListener('click', taskListener)
-      }
-      if (target.dataset.type === 'save') {
-        const title = $task.querySelector('input').value
-        update({ title, id }).then(() => {
-          $task.innerHTML = initialHtml
-          $task.querySelector('span').innerText = title
-          $task.querySelector('[data-type=edit]').dataset.title = title
-          $task.removeEventListener('click', taskListener)
-        })
-      }
+    if (title) {
+      edit({ id, title }).then(() => {
+        location.reload()
+      })
     }
-
-    $task.addEventListener('click', taskListener)
   }
 })
 
-async function update(newNote) {
+async function remove(id) {
+  await fetch(`/${id}`, { method: 'DELETE' })
+}
+
+async function edit(newNote) {
   await fetch(`/${newNote.id}`, {
     method: 'PUT',
     headers: {
@@ -50,8 +31,4 @@ async function update(newNote) {
     },
     body: JSON.stringify(newNote),
   })
-}
-
-async function remove(id) {
-  await fetch(`/${id}`, { method: 'DELETE' })
 }
